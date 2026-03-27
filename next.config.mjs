fa5a6b889f06ca7bd5ae1b300d9824eee63c738e
yeon-ux <yeon-ux@users.noreply.github.com>
2026-03-27 20:42:36 +0900

    Initial commit for reservation platform

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Cloudflare Pages (Edge) 호환 설정
+  experimental: {
+    runtime: 'edge', // 모든 페이지를 Edge Runtime에서 실행하도록 강제
+  },
+};
 
 export default nextConfig;
